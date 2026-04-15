6d6819d0283b283371d841b54034da24cb438a6b
Muramasa <128404768+farhhh@users.noreply.github.com>
2026-04-15 14:22:31 +0300

    Update ЗАГЛУШКА.ps1

--- Scripts/ЗАГЛУШКА.ps1
+++ Scripts/ЗАГЛУШКА.ps1
@@ -1,4 +1,4 @@
-Write-Host "WiApIn Test: Скрипт успешно запущен!"
+Write-Host "проверка динамики 1 WiApIn Test: Скрипт успешно запущен!"
 $path = "$env:USERPROFILE\Desktop\WiApIn_Test.log"
 "Тест пройден $(Get-Date)" | Out-File -FilePath $path
 exit 0
